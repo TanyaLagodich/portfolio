244dd4df22527ddbecb4da42fdeb26967a10013a
Tanya Lagodich <tanyalagodichka@gmail.com>
2022-04-14 20:52:26 +0300

    add change theme

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,14 @@
-console.log('hello');
+window.addEventListener('load', () => {
+
+
+    const changeTheme = () => {
+        const body = document.querySelector('body');
+        body.classList.toggle('dark-theme');
+        console.log(body.classList);
+    }
+
+
+    const changeThemeBtn = document.querySelector('.header__theme-button');
+    changeThemeBtn.addEventListener('click', changeTheme);
+
+});
